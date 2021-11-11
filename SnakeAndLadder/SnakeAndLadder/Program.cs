280f8e3e01c14b7b495ce669334f35757b629217
unknown <Saitharala@gmail.com>
2021-11-12 00:26:10 +0530

    [ Tharala SaiPrashanth] Added UC1-Starting Point Feauture

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder Game.");
+            SnakeAndLadder player = new SnakeAndLadder();
+            player.DieRoll();
+            player.Check();
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class SnakeAndLadder
+    {
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WIN_POSITION = 100, START = 0;
+        public int playerPosition = 0, count = 0;
+        Random random = new Random();
+        public int DieRoll()
+        {
+            count++;
+            int dice = random.Next(1, 7);
+            return dice;
+        }
+        public void Check()
+        {
+            while (playerPosition < WIN_POSITION)
+            {
+                int dieRoll = this.DieRoll();
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case SNAKE:
+                        this.playerPosition -= dieRoll;
+                        if (playerPosition < START)
+                            this.playerPosition = START;
+                        break;
+                    case LADDER:
+                        this.playerPosition += dieRoll;
+                        if (playerPosition > WIN_POSITION)
+                            this.playerPosition -= dieRoll;
+                        break;
+                    case NO_PLAY:
+                        break;
+                }
+                Console.WriteLine("Current Player Position is :" + playerPosition + " after " + "Die roll of:" + dieRoll);
+            }
+            Console.WriteLine("Current Player Position is :" + playerPosition + " and " + "No of Times Played is:" + count);
+        }
+    }
+}
